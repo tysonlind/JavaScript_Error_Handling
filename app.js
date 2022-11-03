647cfeacfa200ee98bae8b30fd6ae1599d9728de
tyson lind <tysonfloydlind@gmail.com>
2022-11-03 11:52:52 -0600

    completed assignment

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+
+  try {
+    let errorCatcher = JSON.parse(data);
+   return errorCatcher;
+  } 
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
+  catch (error){
+    console.error(error.message);
+    return null;
+  }
+  
+  
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
